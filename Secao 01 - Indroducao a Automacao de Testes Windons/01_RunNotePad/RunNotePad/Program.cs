28efb85373f0cdd79e2b0ebef801a7e47fbd52b2
RafahsCunha <rafaelhsc20@gmail.com>
2023-06-14 22:40:47 -0300

    Manipulando Bloco de Notas e Calculadora com WinAppDriver

---
+++ Secao 01 - Indroducao a Automacao de Testes Windons/01_RunNotePad/RunNotePad/Program.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunNotePad
+{
+    class Program
+    {
+        //MANIPULANDO BLOCO DE NOTAS E SALVANDO PRINT DA TELA - OBS PRIMEIRO RODAR WinAppDriver.exe
+        static void Main(string[] args)
+        {
+            WindowsDriver<WindowsElement> notePadSession; //Objeto WindwosDriver com o tipo genérico WindowsElement 
+            AppiumOptions desiredCapabilities = new AppiumOptions();
+            desiredCapabilities.AddAdditionalCapability("app", @"C:\Windows\system32\notepad.exe");
+
+            notePadSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), desiredCapabilities);//Porta onde o WinAppDriver está rodando, :4723
+
+            if (notePadSession == null)
+            {
+                Console.WriteLine("O App não foi iniciado!! ");
+                return;
+            }
+
+            Console.WriteLine($"Titulo da aplicação: {notePadSession.Title}");
+
+            notePadSession.Manage().Window.Maximize();
+
+            var screenShot = notePadSession.GetScreenshot();
+            screenShot.SaveAsFile($".\\Screenshot{DateTime.Now.ToString("ddMMyyyyhhmmss")}.png",
+                OpenQA.Selenium.ScreenshotImageFormat.Png);
+
+            notePadSession.Quit();
+
+
+
+        }
+    }
+}
---
+++ Secao 01 - Indroducao a Automacao de Testes Windons/02_RunCalculator/02_RunCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunNotePad
+{
+    class Program
+    {
+        //MANIPULANDO CALCULADORA SEM EXECUTAR O WinAppDriver.exe EXPLICITAMENTE
+        static void Main(string[] args)
+        {
+            var appiumLocalService = new AppiumServiceBuilder().UsingPort(4723).WithLogFile(new FileInfo(@"C:\Users\Rafa\Desktop\FACULDADE\Onboarding Totvs\Curso Testes Automatizados - Udemy\RunNotePad\RunNotePad\bin\Debug\TestLog.txt")).Build(); // Cria e Salva o log da execução do sistema em um arquivo .txt na pasta Debug
+
+            appiumLocalService.Start();
+
+            AppiumOptions ao = new AppiumOptions();
+
+            ao.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"); //Executa a canculadora
+
+            WindowsDriver<WindowsElement> session = new WindowsDriver<WindowsElement>(appiumLocalService, ao);
+
+            session.Quit();
+        }
+
+    }
+}
