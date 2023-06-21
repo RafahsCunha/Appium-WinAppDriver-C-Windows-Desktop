77b5e906736a4687f9ddacdd24fae3877ddcf042
RafahsCunha <rafaelhsc20@gmail.com>
2023-06-20 21:47:17 -0300

    Adicionando SendKeys e timer de execução de operações matemáticas

--- Secao 02 - Como localizar e manipular elementos/CalcRunnerAppiumV4/CalcRunnerAppiumV4/Program.cs
+++ Secao 02 - Como localizar e manipular elementos/CalcRunnerAppiumV4/CalcRunnerAppiumV4/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium.Appium;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,8 @@ namespace CalcRunnerAppiumV4
             appOptions.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"); //ID da calculadora encontrado em propriedades WinAppDriverUiRecorder XML Nodes
 
             sessionCalculator = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"),appOptions);
+
+            sessionCalculator.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); // Define o tempo de inicialização de cada função, 2 segundos
 
            // Operação da calculadora
 
@@ -48,6 +51,29 @@ namespace CalcRunnerAppiumV4
             {
                 Console.WriteLine("O resultado está Incorreto!!! ");
             }
+
+
+            // Calculando soma com método SendKeys(), mesma função do Click()
+
+            resultadotxt.SendKeys(Keys.Escape);
+
+            resultadotxt.SendKeys("3");
+            resultadotxt.SendKeys("+");
+            resultadotxt.SendKeys("3");
+            resultadotxt.SendKeys("=");
+
+            Console.WriteLine($"Valor mostrado no calculo: {resultadotxt.Text}");
+
+            if (resultadotxt.Text.EndsWith("4"))
+            {
+                Console.WriteLine("O resultado está correto! ");
+            }
+            else
+            {
+                Console.WriteLine("O resultado está Incorreto!!! ");
+            }
+
+
         }
     }
 }
