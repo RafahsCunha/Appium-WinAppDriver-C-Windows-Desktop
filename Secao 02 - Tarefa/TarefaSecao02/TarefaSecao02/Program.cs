0e689dd1047b1ca6db67f1427c62802e79e0bc3e
RafahsCunha <rafaelhsc20@gmail.com>
2023-06-26 21:56:16 -0300

    Iniciando o aplicativo do Windows Filmes e TV

---
+++ Secao 02 - Tarefa/TarefaSecao02/TarefaSecao02/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefaSecao02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsDriver<WindowsElement> filmesTvs;
+            AppiumOptions appOptions = new AppiumOptions();
+
+            appOptions.AddAdditionalCapability("app", "Microsoft.ZuneVideo_8wekyb3d8bbwe!Microsoft.ZuneVideo");
+
+            filmesTvs = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), appOptions);
+        }
+    }
+}
