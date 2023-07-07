2f26a1bbeedb0569c23190e10cfdc3f88559a2a3
RafahsCunha <rafaelhsc20@gmail.com>
2023-07-06 22:44:59 -0300

    xUnit Framework com AppiumWinDriver

---
+++ Secao 04 - Unit Test Framework for Automation/TestProject1/TestProject1/Relogio_Alarme.cs
@@ -0,0 +1,25 @@
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using Xunit;
+
+
+namespace TestProject1
+{
+    public class Relogio_Alarme
+    {
+        [Fact]
+        [Obsolete] // Correção
+        public void TesteExecRelogioeAlarme()
+        {
+            WindowsDriver<WindowsElement> sessionCalculator;
+
+            AppiumOptions appOptions = new AppiumOptions();
+
+            appOptions.AddAdditionalCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App"); //ID do Alarme
+
+            sessionCalculator = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appOptions);
+        }
+    }
+}
